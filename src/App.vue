c2a44c879a85a62396c54b29f05d8e9a10a802f6
bradleyaellis <bradleyaellis@gmail.com>
2020-08-23 14:50:50 -0500

    Initial upload

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <form>
+            <div class="row" id="formDiv" v-if="!formSubmitted">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <form>
+                        <fullName v-model="fullName" @sendName="data"></fullName>
+
+                        <div class="form-group">
+                            <label for="email"> Email </label>
+                                <input type="text" id="email" v-model="email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="password"> Password </label>
+                                <input type="password" id="password" v-model="password" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="storeData"> Yes </label>
+                                <input type="radio" id="yes" value=true v-model="storeData">
+                            <label for="storeData"> No </label>
+                                <input type="radio" id="no" value=false v-model="storeData">
+                        </div>
+                        <hr>
+                        <button class="btn btn-primary" @click="formSubmitted = true"> Submit </button>
+                    </form>
+
+                    <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="formSubmitted" id="showData">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{ fullName }} </p>
+                        <p>Mail: {{ email }} </p>
+                        <p>Password: {{ password }} </p>
+                        <p>Store in Database?: {{ formSubmitted }} </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        components: { FullName: () => import("./components/fullName")},
+        data() {
+            return {
+                fullName: "",
+                email: "",
+                password: "",
+                storeData: false,
+                formSubmitted: false
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/fullName.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div id="firstName">
+      <label for="firstName"> First Name </label>
+        <input type="text" @change="sendName" v-model="firstName">
+    </div>
+
+    <div id="middleInitial">
+      <label for="firstName"> Middle Initial </label>
+        <input type="text" @change="sendName" v-model="middleInitial" rows="4">
+    </div>
+
+    <div id="lastName">
+      <label for="firstName"> Last Name </label>
+        <input type="text" @change="sendName" v-model="lastName">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data (){
+    return {
+      firstName: "",
+      middleInitial: "",
+      lastName: ""
+    }
+  },
+  methods: {
+    sendName(){
+      const fullName = `${this.firstName} ${this.middleInitial} ${this.lastName}`;
+      this.$emit('input', fullName)
+    }
+  }
+}
+</script>
